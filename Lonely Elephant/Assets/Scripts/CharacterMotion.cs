dc48ca5f28b427e2a2ea5170e3aa4d532878ad2b
YHuang <40168804+IRidium083@users.noreply.github.com>
2021-11-14 14:06:52 +0800

    sample scene
    add package:
    terrain tools, Cinemachine.
    simple character control

---
+++ Lonely Elephant/Assets/Scripts/CharacterMotion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotion : MonoBehaviour
+{
+    CharacterController cc;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20f;
+
+    Vector3 moveDir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDir *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDir.y = jumpSpeed;
+            }
+
+        }
+        else
+        {
+
+        }
+        moveDir.y -= gravity * Time.deltaTime;
+        cc.Move(moveDir * Time.deltaTime);
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            float xScale = Mathf.Abs(transform.localScale.x);
+            if (Input .GetAxis("Horizontal") < 0)
+            {
+                transform.localScale = new Vector3(-xScale, transform.lossyScale.y, transform.lossyScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(xScale, transform.lossyScale.y, transform.lossyScale.z);
+            }
+        }
+    }
+}
